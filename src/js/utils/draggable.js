a45fcfaac4c78b7e253d7bab999bd50a160ce9b7
DuhyeongKim <dduskim@gmail.com>
2015-10-05 17:37:50 +0900

    TouchEvent 버그 수정

--- src/js/utils/draggable.js
+++ src/js/utils/draggable.js
@@ -71,7 +71,7 @@
         },
         _calOffset: function(e) {
             var point = (e.touches && e.touches.length > 0) ? e.touches[0] : e;
-            if (point instanceof TouchEvent) {
+            if (TouchEvent && point instanceof TouchEvent) {
                 point = point.changedTouches[0];
             }
 
